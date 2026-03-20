1b0a04975005fd73237fb5bc9b86496d1701b853
yuvan <yuvan9404@gmail.com>
2026-03-20 07:53:33 +0530

    new file logicaloperator.py

---
+++ conditionalstatements.py
@@ -0,0 +1,13 @@
+#lesson 8 conditional statements like if ,if-else, else ,else-if
+
+age=int(input("Enter your age:"))
+
+if age>=18:
+    print("you are eligible to vote")
+    print("pls vote")
+elif age>=60:
+    print("you are an senior citizen")
+else:
+    print("you are not eligible to vote")
+
+# if, elif,else
--- logicaloperator.py
+++ logicaloperator.py
@@ -3,7 +3,6 @@
 #and , or ,not
 
 numbers=100
-
 print(numbers >10 and numbers <101)
 print(numbers >=10 or numbers <=101)
 print(not numbers==100)
