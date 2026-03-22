4663b87eb3c63b437799bb05f6f91e893a5198f1
yuvan <yuvan9404@gmail.com>
2026-03-22 22:02:43 +0530

    range functions 1st commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+#lesson 14 function is a set of reusable code
+
+#syantax def fun_name():
+
+def welcome_msg():
+    print("Welcome to the game")
+    print("my dear nigga")
+welcome_msg()
+
+#coding functions using argument
+def adding_numbers(num1,num2):
+    print(num1+num2)
+adding_numbers(6,2)
