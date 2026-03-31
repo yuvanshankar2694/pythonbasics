f523117102d97d994ec140334897a3d013fee6ba
yuvan <yuvan9404@gmail.com>
2026-03-31 13:00:52 +0530

    added some new list methods

--- listmethods.py
+++ listmethods.py
@@ -1,27 +1,53 @@
 #lesson 11 list and its methods
 
 #append is used to add value at the end of the list
-list=["rib","heart","lungs","spinal cord","liver"]
-list.append("brian")
-print(list)
+lists=["rib","heart","lungs","spinal cord","liver"]
+lists.append("brian")
+print(lists)
 
 #insert is used to add values between the lists
-list.insert(2,"skull")
-print(list)
+lists.insert(2,"skull")
+print(lists)
 
 #remove is used to remove the values in the list we need to give value that will find out and the
 # and removes the value in the list
-list.remove("liver")
-list.remove("spinal cord")
-print(list)
+lists.remove("liver")
+lists.remove("spinal cord")
+print(lists)
 
 #clear method is used to remove all from the list
 # list.clear()
 # print(list)
 
 #reverse method is used to reverse the list
-list.reverse()
-print(list)
+lists.reverse()
+print(lists)
 
 # to return the length of the functions
-print(len(list))
+print(len(lists))
+
+#clear the list
+# list.clear()
+
+#copy list
+copy_list:list[str]=lists.copy()
+lists.remove("rib")
+print(lists)
+print(copy_list)
+
+#count the occurances of the objects
+occ=lists.count("skull")
+print(occ)
+
+#extend or 2 lists
+list1=["kidney","nerves"]
+lists.extend(list1)
+print(lists)
+
+#insert at specific position
+lists.insert(2,"teeth")
+print(lists)
+
+#sort the lists
+lists.sort()
+print(lists)
